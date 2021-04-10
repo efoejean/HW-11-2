2c54b6e1ead08df255b61f7d9316a60a7a2f0463
efoejean <efoejean@gmail.com>
2021-04-10 17:58:00 -0500

    Listen to the input and change the p text color

--- scripts.js
+++ scripts.js
@@ -5,3 +5,8 @@ document
   .addEventListener('keyup', event => {
     p.innerHTML = event.target.value;
   });
+document
+  .querySelector('input[type="color"]')
+  .addEventListener('input', event => {
+    p.style.color = event.target.value;
+  });
